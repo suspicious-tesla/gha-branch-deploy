4e3fa239d971a1472a45fccf17bdb60f5e339bec
James Haughey <jameshaughey@github.com>
2023-02-04 23:12:48 +0000

    Add backend state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.8"
     }
   }
+
+  backend "s3" {
+    bucket = "cirrius-backend-tfstate-global"
+    key    = "suspicious-tesla/terraform/gha-branch-deploy/terraform.tfstate"
+    region = "eu-west-1"
+  }
 }
 
 # Configure the GitHub Provider
